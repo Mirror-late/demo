b80ebc825cf18b8e3c11abf2ff3fc9156dc1e72a
pengjl <pengjl@abc.com>
2022-07-02 22:33:47 +0800

    框架丰富

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,16 +3,20 @@ module.exports = {
       logo: '/assets/img/hero.png',
       nav: [
         { text: '首页', link: '/' },
-        { text: 'About', link: '/about/',target:'_blank' },
+        { text: '项目记录', link: '/about/'},
         {
-            text: '学习',
-            ariaLabel: 'Language Menu',
+            text: '学习笔记',
+            ariaLabel: 'Study',
             items: [
-              { text: '前端', link: '/language/chinese/' },
-              { text: '其他', link: '/language/japanese/' }
+              { text: '前端', link: '/Study/qianduan/' },
+              { text: '其他', link: '/Study/qita/' }
             ]
           },
         { text: 'GitHub', link: 'https://github.com/Mirror-late/',target:'_blank'},
+      ],
+      sidebar:[
+        '',
+        'about'
       ]
     }
   }
